2ab6a0826dc5a2bba4b493c68f3ddedd0d67fcc8
Dikeledi-M <2769106@students.wits.ac.za>
2026-03-02 16:52:11 +0200

    improved size of the country image

--- script.js
+++ script.js
@@ -6,52 +6,48 @@ const errorMessage = document.getElementById('error-message');
 const loadingSpinner = document.getElementById('loading-spinner');
 
 async function searchCountry(countryName) {
-    try{
-        //show loading spinner
-        loadingSpinner.classList.remove('hidden');
-         // Fetch country data
-        const response = await fetch('https://restcountries.com/v3.1/name/${countryName}');
+    try {
+        
+
+        const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
         const data = await response.json();
         const country = data[0];
 
-        //DOM UPDATE
         countryInfo.innerHTML = `
             <h2>${country.name.common}</h2>
             <p><strong>Capital:</strong> ${country.capital[0]}</p>
             <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
             <p><strong>Region:</strong> ${country.region}</p>
-            <img src="${country.flags.svg}" alt="${country.name.common} flag">
+            <img src="${country.flags.svg}" alt="${country.name.common} flag" width="300">
         `;
 
-        // Fetch bordering countries
-        if (country.borders){
-            for (let c_code of country.borders){
-                const response = await fetch('https://restcountries.com/v3.1/alpha/{c_code}');
+        if (country.borders) {
+            for (let c_code of country.borders) {
+                const response = await fetch(`https://restcountries.com/v3.1/alpha/${c_code}`);
                 const data = await response.json();
                 const bordering_country = data[0];
-                
-                 // update bordering countries section
+
                 const article = document.createElement('article');
-                article.innerHTML =  `
+                article.innerHTML = `
                     <p>${bordering_country.name.common}</p>
                     <img src="${bordering_country.flags.svg}" width="100">
                 `;
                 borderingCountries.appendChild(article);
-
             }
+        } else {
+            borderingCountries.innerHTML = '<p>No bordering countries</p>';
         }
-        else{
-            borderingCountries.innerHTML = '<p>No bordering countries';
-        }
-        
-    }
 
-    catch{
+    } catch (error) {
         errorMessage.textContent = error.message;
         errorMessage.classList.remove('hidden');
-
-    }
-    finally{
+    } finally {
         loadingSpinner.classList.add('hidden');
     }
 }
+
+searchBtn.addEventListener('click', () => searchCountry(countryInput.value));
+
+countryInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') searchCountry(countryInput.value);
+});
