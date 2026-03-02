e68a9d982cbc1b4c7f8878f9051974e49d756568
Dikeledi-M <2769106@students.wits.ac.za>
2026-03-02 16:14:53 +0200

    Fetch bordering countries and update the corresponding html section

--- script.js
+++ script.js
@@ -12,7 +12,39 @@ async function searchCountry(countryName) {
          // Fetch country data
         const response = await fetch('https://restcountries.com/v3.1/name/${countryName}');
         const data = await response.json();
+        const country = data[0];
+
+        //DOM UPDATE
+        countryInfo.innerHTML = `
+            <h2>${country.name.common}</h2>
+            <p><strong>Capital:</strong> ${country.capital[0]}</p>
+            <p><strong>Population:</strong> ${country.population.toLocaleString()}</p>
+            <p><strong>Region:</strong> ${country.region}</p>
+            <img src="${country.flags.svg}" alt="${country.name.common} flag">
+        `;
+
+        // Fetch bordering countries
+        if (country.borders){
+            for (let c_code of country.borders){
+                const response = await fetch('https://restcountries.com/v3.1/alpha/{c_code}');
+                const data = await response.json();
+                const bordering_country = data[0];
+                
+                 // update bordering countries section
+                const article = document.createElement('article');
+                article.innerHTML =  `
+                    <p>${bordering_country.name.common}</p>
+                    <img src="${bordering_country.flags.svg}" width="100">
+                `;
+                borderingCountries.appendChild(article);
+
+            }
+        }
         
+    }
+
+    catch{
+
     }
     
 }
